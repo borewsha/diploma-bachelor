49e3ecb5094113c15978e5c763ad28731b99d087
markov-avl <70623307+markov-avl@users.noreply.github.com>
2023-06-05 15:55:34 +1000

    Добавлен переключатель аутентификации (#17)

--- server/src/main/java/com/trip/server/configuration/SecurityConfiguration.java
+++ server/src/main/java/com/trip/server/configuration/SecurityConfiguration.java
@@ -4,11 +4,13 @@ import com.trip.server.entrypoint.*;
 import com.trip.server.filter.*;
 import com.trip.server.handler.*;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.http.*;
 import org.springframework.security.config.annotation.method.configuration.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.*;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.*;
@@ -16,16 +18,19 @@ import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.*;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration {
 
-    private JwtFilter jwtFilter;
+    @Value("${spring.security.authenticated}")
+    private boolean securityAuthenticated;
 
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtFilter jwtFilter;
 
-    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -42,13 +47,24 @@ public class SecurityConfiguration {
                 .accessDeniedHandler(jwtAccessDeniedHandler)
                 .and().cors()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests(ar -> ar
-                        .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .antMatchers(HttpMethod.POST, "/api/registration", "/api/login", "/api/refresh").permitAll()
-                        .anyRequest().permitAll() // .authenticated()
-                        .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                )
+                .and().authorizeRequests(this::setAuthorizeRequestsCustomizer)
                 .build();
     }
 
+    private void setAuthorizeRequestsCustomizer(
+            ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry authorizeRequestsCustomizer
+    ) {
+        authorizeRequestsCustomizer
+                .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/registration", "/api/login", "/api/refresh").permitAll();
+
+        if (securityAuthenticated) {
+            authorizeRequestsCustomizer.anyRequest().authenticated();
+        } else {
+            authorizeRequestsCustomizer.anyRequest().permitAll();
+        }
+
+        authorizeRequestsCustomizer
+                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }

8b581e903a5b35699cbe46212e3c545aa7982756
markov-avl <70623307+markov-avl@users.noreply.github.com>
2023-06-13 23:44:32 +1000

    Изменен алгоритм поиска и возвращена валидация (#18)

--- server/src/main/java/com/trip/server/overpass/model/GeoFilters.java
+++ server/src/main/java/com/trip/server/overpass/model/GeoFilters.java
@@ -3,6 +3,7 @@ package com.trip.server.overpass.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 import java.util.List;
 
@@ -11,8 +12,10 @@ import java.util.List;
 @AllArgsConstructor
 public class GeoFilters {
 
+    @Nullable
     private List<Double> bbox;
 
+    @Nullable
     private Integer radius;
 
 }
